63e81a6211628dea53f70030af6f391149d71045
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-09-10 02:06:53 +0500

    main

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 import { catsData } from './data.js';
 const emotionRadio = document.getElementById('emotion-radios');
+const getImageBtn = document.getElementById('get-image-btn')
+/*
+Challenge:
+1. Set up an eventlistener which calls a new
+   function called "getMatchingCatsArray" when
+   the "Get Image" button is clicked.
+2. getMatchingCatsArray should save the value
+   of the checked radio input to a const and 
+   log out that const.
+*/
+getImageBtn.addEventListener('click',getMatchingCatsArray)
+function getMatchingCatsArray(e){
+    const input= document.querySelector('input[type="radio"]:checked').value
+    console.log(input);
+    
+}
+
 emotionRadio.addEventListener('change', highlightCheckedOption);
 function highlightCheckedOption(e) {
-	/*
-Challenge:
-1. Create an array of all items that have 
-   the "radio" class.
-2. Iterate over the array and remove the 
-   "highlight" class from each one.
-*/
-
 	const radios = document.getElementsByClassName('radio');
 	for (let radio of radios) {
 		radio.classList.remove('highlight');
