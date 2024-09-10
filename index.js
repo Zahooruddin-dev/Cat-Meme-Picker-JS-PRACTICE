5b657a9cf013f0e5b8dc6eb67e2f139fb50459dc
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-09-10 21:20:46 +0500

    close button on the meme event listners

--- index.js
+++ index.js
@@ -3,11 +3,20 @@ import { catsData } from '/data.js';
 const emotionRadios = document.getElementById('emotion-radios');
 const getImageBtn = document.getElementById('get-image-btn');
 const gifsOnlyOption = document.getElementById('gifs-only-option');
-const memeModalInner = document.getElementById('meme-modal-inner')
-const memeModal = document.getElementById('meme-modal')
+const memeModalInner = document.getElementById('meme-modal-inner');
+const memeModal = document.getElementById('meme-modal');
+const memeModalCloseBtn = document.getElementById('meme-modal-close-btn');
 emotionRadios.addEventListener('change', highlightCheckedOption);
 
 getImageBtn.addEventListener('click', renderCat);
+memeModalCloseBtn.addEventListener('click', ()=>{
+    memeModal.style.display= 'none'
+})
+/*
+Challenge:
+1. Wire up the X button in the modal so
+   it closes the modal.
+*/ 
 
 function highlightCheckedOption(e) {
 	const radios = document.getElementsByClassName('radio');
@@ -38,35 +47,22 @@ function getMatchingCatsArray() {
 function getSingleCatObject() {
 	const catsArray = getMatchingCatsArray();
 	if (catsArray.length === 1) {
-		return (catsArray[0]);
+		return catsArray[0];
+	} else {
+		const ran = Math.floor(Math.random() * catsArray.length);
+		return catsArray[ran];
 	}
-    else{
-        const ran= Math.floor(Math.random() * catsArray.length)
-        return (catsArray[ran]);
-        
-    }
 }
 
-function renderCat(){
-    const catObject = getSingleCatObject()
-    memeModalInner.innerHTML=     `<img 
+function renderCat() {
+	const catObject = getSingleCatObject();
+	memeModalInner.innerHTML = `<img 
     class="cat-img" 
     src="./images/${catObject.image}"
     alt="${catObject.alt}"
-    >`
-    memeModal.style.display = 'flex'    /*
-    Challenge:
-    1. Take the object that is returned by 
-       getSingleCatObject and save it to a const 
-       called "catObject".
-    2. Set memeModalInner’s innerHTML to the HTML 
-       string below, remembering to insert the relevant 
-       data from catObject to replace the UPPERCASE text.
-    3. Set memeModal’s display property to "flex". 
-     
-       
-    */ 
-    }
+    >`;
+	memeModal.style.display = 'flex';
+}
 function getEmotionsArray(cats) {
 	const emotionsArray = [];
 	for (let cat of cats) {
