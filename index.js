3d9bc1d22a96a1c2e395182aca06678fb09ae80f
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-09-10 21:23:55 +0500

    fixed order bug

--- index.js
+++ index.js
@@ -9,14 +9,31 @@ const memeModalCloseBtn = document.getElementById('meme-modal-close-btn');
 emotionRadios.addEventListener('change', highlightCheckedOption);
 
 getImageBtn.addEventListener('click', renderCat);
-memeModalCloseBtn.addEventListener('click', ()=>{
-    memeModal.style.display= 'none'
-})
-/*
-Challenge:
-1. Wire up the X button in the modal so
-   it closes the modal.
-*/ 
+memeModalCloseBtn.addEventListener('click', () => {
+	memeModal.style.display = 'none';
+});
+
+function renderCat() {
+	const catObject = getSingleCatObject();
+	memeModalInner.innerHTML = `<img 
+    class="cat-img" 
+    src="./images/${catObject.image}"
+    alt="${catObject.alt}"
+    >`;
+	memeModal.style.display = 'flex';
+}
+
+
+function getSingleCatObject() {
+	const catsArray = getMatchingCatsArray();
+	if (catsArray.length === 1) {
+		return catsArray[0];
+	} else {
+		const ran = Math.floor(Math.random() * catsArray.length);
+		return catsArray[ran];
+	}
+}
+
 
 function highlightCheckedOption(e) {
 	const radios = document.getElementsByClassName('radio');
@@ -42,26 +59,6 @@ function getMatchingCatsArray() {
 		});
 		return matchingCatsArray;
 	}
-}
-
-function getSingleCatObject() {
-	const catsArray = getMatchingCatsArray();
-	if (catsArray.length === 1) {
-		return catsArray[0];
-	} else {
-		const ran = Math.floor(Math.random() * catsArray.length);
-		return catsArray[ran];
-	}
-}
-
-function renderCat() {
-	const catObject = getSingleCatObject();
-	memeModalInner.innerHTML = `<img 
-    class="cat-img" 
-    src="./images/${catObject.image}"
-    alt="${catObject.alt}"
-    >`;
-	memeModal.style.display = 'flex';
 }
 function getEmotionsArray(cats) {
 	const emotionsArray = [];
